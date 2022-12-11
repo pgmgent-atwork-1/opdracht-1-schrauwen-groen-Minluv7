3f983a799a6c2941f8b12675d2a08132af222709
Nese Yildirim <neseyild@student.arteveldehs.be>
2022-12-11 11:47:01 +0100

    remove test doc

--- data/test.js
+++
@@ -1,3 +0,0 @@
-const animals = ["ant", "bison", "camel", "duck", "elephant"];
-const selectedAnimals = animals.slice(0, 4); // The array selectedAnimals contains ['camel', 'duck']
-console.log(selectedAnimals);
